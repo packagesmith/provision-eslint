01d4b859f001d3c961d691da7af9f30644241054
Keith Cirkel <github@keithcirkel.co.uk>
2016-01-22 17:11:24 +0000

    feat: initial implementation

---
+++ src/index.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+import defaultsDeep from 'lodash.defaultsdeep';
+import jsonFile from 'packagesmith.formats.json';
+import { runProvisionerSet } from 'packagesmith';
+import sortPackageJson from 'sort-package-json';
+const presetOptions = {
+  'strict': '^8.2.0',
+  'xo': '^0.10.1',
+  'google': '^0.4.0',
+  'standard': '^5.1.0',
+  'airbnb': '^5.0.1',
+  'semistandard': '^5.0.0',
+  'defaults': '^9.0.0',
+  'strongloop': '^1.0.2',
+  'simplified': '^1.2.2',
+};
+export function provisionEslint({
+  eslintConfig = false,
+  scriptName = 'lint',
+  presets,
+  pretest = false,
+  formatScriptName = false,
+} = {}) {
+  const presetsQuestion = {
+    type: 'list',
+    name: 'eslintPreset',
+    message: 'Which preset would you like to use for eslint?',
+    choices: Object.keys(presetOptions).map((name) => ({ name, value: name })),
+    when: (answers) => 'eslintPreset' in answers === false,
+  };
+  return {
+    'package.json': {
+      after: 'npm install',
+      questions: presets ? [] : [ presetsQuestion ],
+      contents: jsonFile((contents, { eslintPreset } = {}) => {
+        let chosenPresets = presets || eslintPreset;
+        if (typeof chosenPresets === 'string') {
+          chosenPresets = { [chosenPresets]: presetOptions[chosenPresets] };
+        }
+        const devDependencies = Object.keys(chosenPresets)
+          .reduce((total, preset) => {
+            total[`eslint-config-${ preset.replace(/^eslint-config/, '') }`] = chosenPresets[preset];
+            return total;
+          }, {});
+        devDependencies.eslint = '^2.1.0';
+        const packageJson = {
+          eslintConfig: eslintConfig || {
+            extends: Object.keys(chosenPresets).map((preset) => preset.replace(/^eslint-config/, '')),
+          },
+          devDependencies,
+          scripts: {
+            [scriptName]: 'eslint $npm_package_directories_src',
+          },
+        };
+        if (pretest) {
+          packageJson.scripts.pretest = `npm run ${ scriptName }`;
+        }
+        if (formatScriptName) {
+          packageJson.scripts[formatScriptName] = 'eslint --fix $npm_package_directories_src';
+        }
+        return sortPackageJson(defaultsDeep(packageJson, contents, {
+          directories: {
+            src: 'src',
+          },
+        }));
+      }),
+    },
+  };
+}
+export default provisionEslint;
+
+if (require.main === module) {
+  const directoryArgPosition = 2;
+  runProvisionerSet(process.argv[directoryArgPosition] || '.', provisionEslint());
+}
---
+++ test/index.js
@@ -0,0 +1,241 @@
+import chai from 'chai';
+chai.should();
+import provisionEslint from '../src/';
+describe('provisionEslint', () => {
+
+  it('returns an object with `package.json`.`contents` function', () => {
+    provisionEslint()
+      .should.be.an('object')
+      .with.keys([ 'package.json' ])
+      .with.property('package.json')
+        .with.keys([ 'contents', 'after', 'questions' ])
+        .with.property('contents')
+          .that.is.a('function');
+  });
+
+  it('returns an object with eslintPreset question if `presets` not given', () => {
+    provisionEslint()['package.json'].questions
+      .should.have.lengthOf(1)
+        .with.property(0)
+          .with.property('name', 'eslintPreset');
+  });
+
+  it('returns an object without eslintPreset question if `presets` given', () => {
+    provisionEslint({ presets: 'foo' })['package.json'].questions
+      .should.have.lengthOf(0);
+  });
+
+  describe('eslintPreset question', () => {
+    let question = null;
+    beforeEach(() => {
+      question = provisionEslint()['package.json'].questions[0];
+    });
+
+    it('has the right properties', () => {
+      question.should.have.keys([ 'type', 'name', 'message', 'choices', 'when' ]);
+    });
+
+    it('includes sensible choices', () => {
+      question.choices.should.deep.contain.members([
+        { name: 'xo', value: 'xo' },
+        { name: 'standard', value: 'standard' },
+        { name: 'strict', value: 'strict' },
+        { name: 'airbnb', value: 'airbnb' },
+      ]);
+    });
+
+  });
+
+  describe('contents function', () => {
+    let subFunction = null;
+    beforeEach(() => {
+      subFunction = provisionEslint()['package.json'].contents;
+    });
+
+    it('adds eslint directives to json, when given answers.eslintPreset', () => {
+      JSON.parse(subFunction('{}', { eslintPreset: 'strict' }))
+        .should.deep.equal({
+          directories: {
+            src: 'src',
+          },
+          devDependencies: {
+            'eslint': '^2.1.0',
+            'eslint-config-strict': '^8.2.0',
+          },
+          eslintConfig: {
+            extends: [
+              'strict',
+            ],
+          },
+          scripts: {
+            'lint': 'eslint $npm_package_directories_src',
+          },
+        });
+    });
+
+    it('adds different `extends` and `devDependencies` when given different preset', () => {
+      JSON.parse(subFunction('{}', { eslintPreset: 'xo' }))
+        .should.deep.equal({
+          directories: {
+            src: 'src',
+          },
+          devDependencies: {
+            'eslint': '^2.1.0',
+            'eslint-config-xo': '^0.10.1',
+          },
+          eslintConfig: {
+            extends: [
+              'xo',
+            ],
+          },
+          scripts: {
+            'lint': 'eslint $npm_package_directories_src',
+          },
+        });
+    });
+
+    it('overrides eslintPreset with `presets` config option', () => {
+      JSON.parse(provisionEslint({ presets: 'xo' })['package.json'].contents('{}'), {})
+        .should.deep.equal({
+          directories: {
+            src: 'src',
+          },
+          devDependencies: {
+            'eslint': '^2.1.0',
+            'eslint-config-xo': '^0.10.1',
+          },
+          eslintConfig: {
+            extends: [
+              'xo',
+            ],
+          },
+          scripts: {
+            'lint': 'eslint $npm_package_directories_src',
+          },
+        });
+    });
+
+    it('accepts custom `presets`', () => {
+      JSON.parse(provisionEslint({ presets: { 'foo': '^1.2.3' } })['package.json'].contents('{}'), {})
+        .should.deep.equal({
+          directories: {
+            src: 'src',
+          },
+          devDependencies: {
+            'eslint': '^2.1.0',
+            'eslint-config-foo': '^1.2.3',
+          },
+          eslintConfig: {
+            extends: [
+              'foo',
+            ],
+          },
+          scripts: {
+            'lint': 'eslint $npm_package_directories_src',
+          },
+        });
+    });
+
+    it('overrides `eslintConfig` with `eslintConfig` option', () => {
+      subFunction = provisionEslint({
+        presets: {},
+        eslintConfig: {
+          rules: {
+            foo: 'bar',
+          },
+        },
+      })['package.json'].contents;
+
+      JSON.parse(subFunction('{}'))
+        .should.deep.equal({
+          directories: {
+            src: 'src',
+          },
+          devDependencies: {
+            'eslint': '^2.1.0',
+          },
+          eslintConfig: {
+            rules: {
+              foo: 'bar',
+            },
+          },
+          scripts: {
+            'lint': 'eslint $npm_package_directories_src',
+          },
+        });
+    });
+
+    it('changes scripts name with `scriptName` option', () => {
+      subFunction = provisionEslint({
+        presets: {},
+        scriptName: 'build:js',
+      })['package.json'].contents;
+
+      JSON.parse(subFunction('{}'))
+        .should.deep.equal({
+          directories: {
+            src: 'src',
+          },
+          devDependencies: {
+            'eslint': '^2.1.0',
+          },
+          eslintConfig: {
+            extends: [],
+          },
+          scripts: {
+            'build:js': 'eslint $npm_package_directories_src',
+          },
+        });
+    });
+
+    it('adds pretest: npm run lint, if config.pretest = true', () => {
+      subFunction = provisionEslint({
+        presets: {},
+        pretest: true,
+      })['package.json'].contents;
+
+      JSON.parse(subFunction('{}'))
+        .should.deep.equal({
+          directories: {
+            src: 'src',
+          },
+          devDependencies: {
+            'eslint': '^2.1.0',
+          },
+          eslintConfig: {
+            extends: [],
+          },
+          scripts: {
+            'lint': 'eslint $npm_package_directories_src',
+            'pretest': 'npm run lint',
+          },
+        });
+    });
+
+    it('adds formatScriptName if present', () => {
+      subFunction = provisionEslint({
+        presets: {},
+        formatScriptName: 'fmt',
+      })['package.json'].contents;
+
+      JSON.parse(subFunction('{}'))
+        .should.deep.equal({
+          directories: {
+            src: 'src',
+          },
+          devDependencies: {
+            'eslint': '^2.1.0',
+          },
+          eslintConfig: {
+            extends: [],
+          },
+          scripts: {
+            'lint': 'eslint $npm_package_directories_src',
+            'fmt': 'eslint --fix $npm_package_directories_src',
+          },
+        });
+    });
+
+  });
+
+});
